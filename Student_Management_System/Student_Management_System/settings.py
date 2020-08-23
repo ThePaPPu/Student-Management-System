9c86159ec8b78d421a35473cae779a56f00cb645
Mr Pappu <58678139+ThePaPPu@users.noreply.github.com>
2020-08-23 17:39:18 +0600

    connect to database
    -create database 'student_management_system'
    -create user 'student_management_system'@'localhost' identified by 'student_management_password';
    -grant all privileges on *.* to 'student_management_system'@'localhost';

--- Student_Management_System/Student_Management_System/settings.py
+++ Student_Management_System/Student_Management_System/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'Student_Management_System.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'Student_Management_System',
+        'USER':'student_management_system',
+        'PASSWORD':'student_management_password',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
