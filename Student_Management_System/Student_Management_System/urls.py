723dd4d14f6e2bd1e6a85bc787e71e6ec419de86
Mr Pappu <58678139+ThePaPPu@users.noreply.github.com>
2020-08-26 23:07:25 +0600

    Write Code for added staff
    stored information into database for added staff

--- Student_Management_System/Student_Management_System/urls.py
+++ Student_Management_System/Student_Management_System/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('get_user_details', views.GetUserDetails),
     path('logout_user', views.LogoutUser),
     path('admin_home', AdminViews.admin_home),
+    path('add_staff', AdminViews.add_staff),
+    path('add_staff_save', AdminViews.add_staff_save),
 
 
 ]+static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
--- Student_Management_System/student_app/AdminViews.py
+++ Student_Management_System/student_app/AdminViews.py
@@ -1,5 +1,37 @@
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+
+from student_app.models import CustomUser
 
 
 def admin_home(request):
     return render(request, "admin_template/home_content.html")
+
+
+def add_staff(request):
+    return render(request, "admin_template/add_staff_template.html")
+
+
+def add_staff_save(request):
+    if request.method != "POST":
+        return HttpResponse("Method not allowed")
+    else:
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        address = request.POST.get("address")
+
+        try:
+            user = CustomUser.objects.create_user(username=username, password=password, email=email, last_name=last_name, first_name=first_name, user_type=2)
+            user.staffs.address=address
+            user.save()
+            messages.success(request, "Successfully Added a Staff")
+            return HttpResponseRedirect("/add_staff")
+        except:
+            messages.error(request, "Failed to Add a Staff")
+            return HttpResponseRedirect("/add_staff")
+
+
