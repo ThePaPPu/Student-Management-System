e6a83ca019315dce3de74cb18272a79f2819c58e
ThePaPPu <pappu170101019@gmail.com>
2020-09-25 12:49:06 +0600

    create EditStudentForm in forms.py
    Standard Django Form

--- Student_Management_System/student_app/AdminViews.py
+++ Student_Management_System/student_app/AdminViews.py
@@ -4,8 +4,9 @@ from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from student_app.forms import AddStudentForm
+from student_app.forms import AddStudentForm, EditStudentForm
 from student_app.models import CustomUser, Courses, Subjects, Staffs, Students
 
 
@@ -36,10 +37,10 @@ def add_course_teacher_save(request):
             user.staffs.address = address
             user.save()
             messages.success(request, "Successfully Added a Course Teacher")
-            return HttpResponseRedirect("/add_course_teacher")
+            return HttpResponseRedirect(reverse("add_course_teacher"))
         except:
             messages.error(request, "Failed to Add  a Course Teacher")
-            return HttpResponseRedirect("/add_course_teacher")
+            return HttpResponseRedirect(reverse("add_course_teacher"))
 
 
 def manage_course_teacher(request):
@@ -49,7 +50,7 @@ def manage_course_teacher(request):
 
 def edit_course_teacher(request, staff_id):
     staff = Staffs.objects.get(admin=staff_id)
-    return render(request, "admin_template/edit_course_teacher_template.html", {"staff": staff, "id":staff_id})
+    return render(request, "admin_template/edit_course_teacher_template.html", {"staff": staff, "id": staff_id})
 
 
 def edit_course_teacher_save(request):
@@ -80,14 +81,11 @@ def edit_course_teacher_save(request):
             staff_model.save()
 
             messages.success(request, "Successfully Edited Course Teacher")
-            return HttpResponseRedirect("/edit_course_teacher/" + staff_id)
+            return HttpResponseRedirect(reverse("edit_course_teacher",kwargs={"staff_id":staff_id}))
 
         except:
             messages.error(request, "Failed to Edit Course Teacher")
-            return HttpResponseRedirect("/edit_course_teacher/" + staff_id)
-
-
-
+            return HttpResponseRedirect(reverse("edit_course_teacher", kwargs={"staff_id":staff_id}))
 
 
 # Courses
@@ -106,10 +104,10 @@ def add_course_save(request):
             course_model = Courses(course_name=course)
             course_model.save()
             messages.success(request, "Successfully Added Course")
-            return HttpResponseRedirect("/add_course")
+            return HttpResponseRedirect(reverse("add_course"))
         except:
             messages.error(request, "Failed to Add Course")
-            return HttpResponseRedirect("/add_course")
+            return HttpResponseRedirect(reverse("add_course"))
 
 
 def manage_course(request):
@@ -119,7 +117,7 @@ def manage_course(request):
 
 def edit_course(request, course_id):
     course = Courses.objects.get(id=course_id)
-    return render(request, "admin_template/edit_course_template.html", {"course": course, "id":course_id})
+    return render(request, "admin_template/edit_course_template.html", {"course": course, "id": course_id})
 
 
 def edit_course_save(request):
@@ -136,18 +134,18 @@ def edit_course_save(request):
             course.save()
 
             messages.success(request, "Successfully Edited  Course")
-            return HttpResponseRedirect("/edit_course/" + course_id)
+            return HttpResponseRedirect(reverse("edit_course",kwargs={"course_id":course_id}))
 
         except:
             messages.error(request, "Failed to Edit Course")
-            return HttpResponseRedirect("/edit_course/" + course_id)
+            return HttpResponseRedirect(reverse("edit_course", kwargs={"course_id":course_id}))
 
 
 # Students
 
 def add_student(request):
     form = AddStudentForm()
-    return render(request, "admin_template/add_student_template.html", {"form":form})
+    return render(request, "admin_template/add_student_template.html", {"form": form})
 
 
 def add_student_save(request):
@@ -189,11 +187,11 @@ def add_student_save(request):
                 user.students.session_end_year = session_end
                 user.save()
                 messages.success(request, "Successfully Added a Student")
-                return HttpResponseRedirect("/add_student")
+                return HttpResponseRedirect(reverse("add_student"))
 
             except:
                 messages.error(request, "Failed to Add  a Student")
-                return HttpResponseRedirect("/add_student")
+                return HttpResponseRedirect(reverse("add_student"))
 
         else:
             form = AddStudentForm(request.POST)
@@ -206,9 +204,24 @@ def manage_student(request):
 
 
 def edit_student(request, student_id):
-    courses = Courses.objects.all()
+    request.session['student_id'] = student_id
     student = Students.objects.get(admin=student_id)
-    return render(request, "admin_template/edit_student_template.html", {"student": student, "courses": courses, "id":student_id})
+
+    form = EditStudentForm()
+    form.fields['email'].initial = student.admin.email
+    form.fields['password'].initial = student.admin.password
+    form.fields['first_name'].initial = student.admin.first_name
+    form.fields['last_name'].initial = student.admin.last_name
+    form.fields['username'].initial = student.admin.username
+    form.fields['address'].initial = student.address
+    form.fields['course'].initial = student.course_id.id
+    form.fields['gender'].initial = student.gender
+    form.fields['session_start'].initial = student.session_start_year
+    form.fields['session_end'].initial = student.session_end_year
+    form.fields['profile_pic'].initial = student.profile_pic
+
+    return render(request, "admin_template/edit_student_template.html",
+                  {"form": form, "id": student_id, "username": student.admin.username})
 
 
 def edit_student_save(request):
@@ -216,56 +229,74 @@ def edit_student_save(request):
         return HttpResponse("<h2>Method not allowed</h2>")
 
     else:
-        student_id = request.POST.get("student_id")
-        first_name = request.POST.get("first_name")
-        last_name = request.POST.get("last_name")
-        email = request.POST.get("email")
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        address = request.POST.get("address")
-        session_start = request.POST.get("session_start")
-        session_end = request.POST.get("session_end")
-        course_id = request.POST.get("course")
-        gender = request.POST.get("gender")
+        student_id = request.session.get("student_id")
 
-        if request.FILES.get('profile_pic',False):
-            profile_pic = request.FILES['profile_pic']
-            fs = FileSystemStorage()
-            filename = fs.save(profile_pic.name, profile_pic)
-            profile_pic_url = fs.url(filename)
+        if student_id is None:
+            return HttpResponseRedirect(reverse("manage_student"))
+
+        form = EditStudentForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            address = form.cleaned_data["address"]
+            session_start = form.cleaned_data["session_start"]
+            session_end = form.cleaned_data["session_end"]
+            course_id = form.cleaned_data["course"]
+            gender = form.cleaned_data["gender"]
+
+            if request.FILES.get('profile_pic', False):
+                profile_pic = request.FILES['profile_pic']
+                fs = FileSystemStorage()
+                filename = fs.save(profile_pic.name, profile_pic)
+                profile_pic_url = fs.url(filename)
+
+            else:
+                profile_pic_url = None
+
+            try:
+                user = CustomUser.objects.get(id=student_id)
+                user.first_name = first_name
+                user.last_name = last_name
+                user.email = email
+                user.username = username
+                user.password = password
+                user.save()
+
+                student = Students.objects.get(admin=student_id)
+                student.address = address
+                student.gender = gender
+                student.session_start_year = session_start
+                student.session_end_year = session_end
+
+                course = Courses.objects.get(id=course_id)
+                student.course_id = course
+
+                if profile_pic_url is not None:
+                    student.profile_pic = profile_pic_url
+
+                student.save()
+
+                del request.session['student_id']
+
+                messages.success(request, "Successfully Edited  Student")
+                return HttpResponseRedirect(reverse("edit_student", kwargs={"student_id":student_id}))
+
+
+            except:
+                messages.error(request, "Failed to Edit Student")
+                return HttpResponseRedirect(reverse("edit_student", kwargs={"student_id":student_id}))
 
         else:
-            profile_pic_url = None
-
-        try:
-            user = CustomUser.objects.get(id=student_id)
-            user.first_name = first_name
-            user.last_name = last_name
-            user.email = email
-            user.username = username
-            user.password = password
-            user.save()
-
+            form = EditStudentForm(request.POST)
             student = Students.objects.get(admin=student_id)
-            student.address = address
-            student.gender = gender
-            student.session_start_year = session_start
-            student.session_end_year = session_end
+            return render(request, "admin_template/edit_student_template.html",
+                  {"form": form, "id": student_id, "username": student.admin.username})
 
-            course = Courses.objects.get(id=course_id)
-            student.course_id = course
 
-            if profile_pic_url is not None:
-                student.profile_pic = profile_pic_url
-
-            student.save()
-
-            messages.success(request, "Successfully Edited  Student")
-            return HttpResponseRedirect("/edit_student/" + student_id)
-
-        except:
-            messages.error(request, "Failed to Edit Student")
-            return HttpResponseRedirect("/edit_student/" + student_id)
 
 
 # Subject
@@ -290,11 +321,11 @@ def add_subject_save(request):
             subject = Subjects(subject_name=subject_name, course_id=course, staff_id=staff)
             subject.save()
             messages.success(request, "Successfully Added Subject")
-            return HttpResponseRedirect("/add_subject")
+            return HttpResponseRedirect(reverse("add_subject"))
 
         except:
             messages.error(request, "Failed to add Subject")
-            return HttpResponseRedirect("/add_subject")
+            return HttpResponseRedirect(reverse("add_subject"))
 
 
 def manage_subject(request):
@@ -307,7 +338,7 @@ def edit_subject(request, subject_id):
     courses = Courses.objects.all()
     staffs = CustomUser.objects.filter(user_type=2)
     return render(request, "admin_template/edit_subject_template.html",
-                  {"subject": subject, "courses": courses, "staffs": staffs, "id":subject_id})
+                  {"subject": subject, "courses": courses, "staffs": staffs, "id": subject_id})
 
 
 def edit_subject_save(request):
@@ -330,8 +361,8 @@ def edit_subject_save(request):
             subject.save()
 
             messages.success(request, "Successfully Edited Subject")
-            return HttpResponseRedirect("/edit_course/" + subject_id)
+            return HttpResponseRedirect(reverse("edit_subject",kwargs={"subject_id":subject_id}))
 
         except:
             messages.error(request, "Failed to Edit Subject")
-            return HttpResponseRedirect("/edit_subject/" + subject_id)
+            return HttpResponseRedirect(reverse("edit_subject",kwargs={"subject_id":subject_id}))
--- Student_Management_System/student_app/forms.py
+++ Student_Management_System/student_app/forms.py
@@ -44,3 +44,45 @@ class AddStudentForm(forms.Form):
     session_end = forms.DateField(label="Session End", widget=DateInput(attrs={"class":"form-control"}))
 
     profile_pic = forms.FileField(label="Image", widget=forms.FileInput(attrs={"class":"form-control"}))
+
+
+
+
+
+class EditStudentForm(forms.Form):
+    email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class":"form-control"}), required=False)
+
+    password = forms.CharField(label="Password",max_length=50,widget=forms.PasswordInput(attrs={"class":"form-control"}), required=False)
+
+    first_name = forms.CharField(label="First Name", max_length=50,
+                                 widget=forms.TextInput(attrs={"class":"form-control"}), required=False)
+
+    last_name = forms.CharField(label="Last Name", max_length=50,
+                                widget=forms.TextInput(attrs={"class":"form-control"}), required=False)
+
+    username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}), required=False)
+
+    address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class":"form-control"}), required=False)
+
+    courses = Courses.objects.all()
+    course_list = []
+    for course in courses:
+        small_course = (course.id, course.course_name)
+        course_list.append(small_course)
+
+    course = forms.ChoiceField(label="Course", choices=course_list,
+                               widget=forms.Select(attrs={"class":"form-control"}), required=False)
+
+    gender_choice = (
+        ("Male", "Male"),
+        ("Female", "Female")
+    )
+
+    gender = forms.ChoiceField(label="Gender", choices=gender_choice,
+                               widget=forms.Select(attrs={"class":"form-control"}), required=False)
+
+    session_start = forms.DateField(label="Session Start", widget=DateInput(attrs={"class":"form-control"}), required=False)
+
+    session_end = forms.DateField(label="Session End", widget=DateInput(attrs={"class":"form-control"}), required=False)
+
+    profile_pic = forms.FileField(label="Image", widget=forms.FileInput(attrs={"class":"form-control"}), required=False)
